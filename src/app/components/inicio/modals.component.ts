3901bee1222c1f354c11f9a0c47ccb348516d92c
Jose Carlos <ljosecarlos295@gmail.com>
2023-01-28 19:21:17 -0500

    add variables en servicio and contruir componentes noticias y modificar componente inicio agregando publicaciones

--- src/app/components/inicio/modals.component.ts
+++ src/app/components/inicio/modals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modals.service';
 declare let $:any;
 
 @Component({
@@ -9,52 +10,31 @@ declare let $:any;
 })
 export class ModalsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modalService: ModalService) { }
 
-  public mostrar1 = true;
-  public mostrar2 = false;
-  public mostrar3 = false;
 
-  public clase1:string = "btn-warning";
-  public clase2:string = "btn-outline-warning";
-  public clase3:string = "btn-outline-warning";
 
   ngOnInit(): void {
   }
 
   cerrarModalTecnologia() {
-    $('#modalTecnologias').modal('hide');
-    
-    setTimeout(()=> {
-      this.pagina1();
-    }, 500)
+    this.modalService.cerrarModalTecnologia();
   }
 
   pagina1() {
-    this.mostrar1 = true;
-    this.mostrar2 = false;
-    this.mostrar3 = false;
-    this.clase1 = "btn-warning";
-    this.clase2 = "btn-outline-warning";
-    this.clase3 = "btn-outline-warning";
+    this.modalService.pagina1()
   }
 
   pagina2() {
-    this.mostrar1 = false;
-    this.mostrar2 = true;
-    this.mostrar3 = false;
-    this.clase1 = "btn-outline-warning";
-    this.clase2 = "btn-warning";
-    this.clase3 = "btn-outline-warning";
+    this.modalService.pagina2();
   }
 
   pagina3() {
-    this.mostrar1 = false;
-    this.mostrar2 = false;
-    this.mostrar3 = true;
-    this.clase1 = "btn-outline-warning";
-    this.clase2 = "btn-outline-warning";
-    this.clase3 = "btn-warning";
+    this.modalService.pagina3();
+  }
+
+  cerrarSobreMi() {
+    this.modalService.cerrarSobreMi();
   }
 
 }
--- src/app/components/noticias/noticias.component.ts
+++ src/app/components/noticias/noticias.component.ts
@@ -8,6 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticiasComponent implements OnInit {
 
+  noticias = [
+    {
+      titulo: 'Lorem ipsum dolor sit amet',
+      subtitulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      fecha: '17/05/2020',
+      img: '../../../assets/img/noticia1.jpg'
+    },
+    {
+      titulo: 'Lorem ipsum dolor sit amet',
+      subtitulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      fecha: '17/05/2020',
+      img: '../../../assets/img/noticia2.jpg'
+    },
+    {
+      titulo: 'Lorem ipsum dolor sit amet',
+      subtitulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      fecha: '17/05/2020',
+      img: '../../../assets/img/noticia3.jpg'
+    },
+    {
+      titulo: 'Lorem ipsum dolor sit amet',
+      subtitulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      fecha: '17/05/2020',
+      img: '../../../assets/img/noticia4.jpg'
+    },
+    {
+      titulo: 'Lorem ipsum dolor sit amet',
+      subtitulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      fecha: '17/05/2020',
+      img: '../../../assets/img/noticia5.jpg'
+    },
+    {
+      titulo: 'Lorem ipsum dolor sit amet',
+      subtitulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      fecha: '17/05/2020',
+      img: '../../../assets/img/noticia6.jpg'
+    },
+    {
+      titulo: 'Lorem ipsum dolor sit amet',
+      subtitulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      fecha: '17/05/2020',
+      img: '../../../assets/img/noticia7.jpg'
+    },
+    {
+      titulo: 'Lorem ipsum dolor sit amet',
+      subtitulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      fecha: '17/05/2020',
+      img: '../../../assets/img/noticia8.jpg'
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
